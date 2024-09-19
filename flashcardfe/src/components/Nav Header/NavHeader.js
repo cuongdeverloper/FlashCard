b11b88b60fea5e54fc1091de89caa591274fbc8c
de180334 <khoa2k4hg@gmail.com>
2024-09-19 22:24:25 +0700

    navbar side bar

--- flashcardfe/src/components/Nav Header/NavHeader.js
+++ flashcardfe/src/components/Nav Header/NavHeader.js
@@ -1,86 +1,118 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { NavLink, useNavigate } from 'react-router-dom';
-import './NavHeader.scss';
-import { IoPersonCircle } from 'react-icons/io5';
-import { GoSearch } from 'react-icons/go';
-import logoImg from '../../assests/OIP.jpg';
-import { IoAddOutline } from 'react-icons/io5';
-import { NavDropdown } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./NavHeader.scss";
+import { IoPersonCircle } from "react-icons/io5";
+import { GoSearch } from "react-icons/go";
+import logoImg from "../../assests/logo.png";
+import { IoAddOutline } from "react-icons/io5";
+import { NavDropdown } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
 
 const NavHeader = () => {
-    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
-    const navigate = useNavigate();
-    const cart1 = useSelector(state => state.cart);  // Adjust according to your state structure
-    const [openDropdown, setOpenDropdown] = useState(null);
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const navigate = useNavigate();
+  const cart1 = useSelector((state) => state.cart); // Adjust according to your state structure
+  const [openDropdown, setOpenDropdown] = useState(null);
 
-    // Function to handle mouse entering a dropdown
-    const handleMouseEnter = (dropdownId) => {
-        setOpenDropdown(dropdownId);
-    };
+  // Function to handle mouse entering a dropdown
+  const handleMouseEnter = (dropdownId) => {
+    setOpenDropdown(dropdownId);
+  };
 
-    // Function to handle mouse leaving a dropdown
-    const handleMouseLeave = () => {
-        setOpenDropdown(null);
-    };
+  // Function to handle mouse leaving a dropdown
+  const handleMouseLeave = () => {
+    setOpenDropdown(null);
+  };
 
-    return (
-        <div className="Header-container" style={{ zIndex: '1000', width: '100%' }}>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container className='container-navbar'>
-                    <Navbar.Brand onClick={() => navigate('/')} style={{ height: '100%', width: 'auto' }}>
-                        <img style={{ height: '100%', width: 'auto' }} src={logoImg} alt="FlashCardLogo Logo" />
-                    </Navbar.Brand>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        {/* <Nav className="me-auto"> */}
-                        <div className='d-flex flex-wrap navbar-outlay'>
-                            {/* Icon Dropdown */}
-                            <div
-                                className="text-white px-3"
-                                onMouseEnter={() => handleMouseEnter('shirt-dropdown')}
-                                onMouseLeave={handleMouseLeave}
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <NavDropdown
-                                    title={
-                                        <span>
-                                            <IoAddOutline style={{ color: openDropdown === 'shirt-dropdown' ? 'yellow' : 'white', fontSize: '24px' }} />
-                                        </span>
-                                    }
-                                    id="shirt-dropdown"
-                                    show={openDropdown === 'shirt-dropdown'}
-                                >
-                                    <NavDropdown.Item as={NavLink} to='/shop/product/shirt'>Class</NavDropdown.Item>
-                                    <NavDropdown.Item as={NavLink} to='/crud-q'>FlashCard/Quiz</NavDropdown.Item>
-                                    <NavDropdown.Item as={NavLink} to='/shop/product/ao-ba-lo'>Folder</NavDropdown.Item>
-                                </NavDropdown>
-                            </div>
-                        </div>
-                        {/* </Nav> */}
-                    </Navbar.Collapse>
-                    <Navbar.Collapse>
-                        <Nav>
-                            {isAuthenticated ? (
-                                <NavLink to='/logout' className='nav-link'>Logout</NavLink>
-                            ) : (
-                                <NavLink to='/login' className='nav-link'>Login</NavLink>
-                            )}
-                            <Nav.Link onClick={() => navigate('/user')}>
-                                <IoPersonCircle />
-                            </Nav.Link>
+  return (
+    <div className="Header-container" style={{ zIndex: "1000", width: "100%" }}>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container className="container-navbar">
+          <Navbar.Brand
+            onClick={() => navigate("/")}
+            style={{ height: "100%", width: "auto" }}
+          >
+            <img
+              style={{ height: "100%", width: "auto" }}
+              src={logoImg}
+              alt="FlashCardLogo Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Collapse id="mid-navbar-nav">
+                  <FaSearch id="search-icon" />
+                  <input placeholder="Searching to faster..."></input>
+            </Navbar.Collapse>
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* <Nav className="me-auto"> */}
+            <div className="d-flex flex-wrap navbar-outlay">
+              {/* Icon Dropdown */}
+              <div
+                className="text-white px-3"
+                onMouseEnter={() => handleMouseEnter("shirt-dropdown")}
+                onMouseLeave={handleMouseLeave}
+                style={{ cursor: "pointer" }}
+              >
+                <NavDropdown
+                  title={
+                    <span>
+                      <IoAddOutline
+                        style={{
+                          color:
+                            openDropdown === "shirt-dropdown"
+                              ? "yellow"
+                              : "white",
+                          fontSize: "24px",
+                        }}
+                      />
+                    </span>
+                  }
+                  id="shirt-dropdown"
+                  show={openDropdown === "shirt-dropdown"}
+                >
+                  <NavDropdown.Item as={NavLink} to="/shop/product/shirt">
+                    Class
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/crud-q">
+                    FlashCard/Quiz
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/shop/product/ao-ba-lo">
+                    Folder
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+            </div>
 
-                            <Nav.Link href="#home">
-                                <GoSearch />
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
-}
+            {/* </Nav> */}
+          </Navbar.Collapse>
+
+          <Navbar.Collapse>
+            <Nav>
+              {isAuthenticated ? (
+                <NavLink to="/logout" className="nav-link">
+                  Logout
+                </NavLink>
+              ) : (
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              )}
+              <Nav.Link onClick={() => navigate("/user")}>
+                <IoPersonCircle />
+              </Nav.Link>
+
+              <Nav.Link href="#home">
+                <GoSearch />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
 export default NavHeader;
--- flashcardfe/src/components/SideBar/Sidebar.js
+++ flashcardfe/src/components/SideBar/Sidebar.js
@@ -1,117 +1,155 @@
-import 'react-pro-sidebar/dist/css/styles.css';
+import "react-pro-sidebar/dist/css/styles.css";
+import { FaHome } from "react-icons/fa";
+import { FaFolderOpen } from "react-icons/fa";
 import {
-    ProSidebar,
-    Menu,
-    MenuItem,
-    SubMenu,
-    SidebarHeader,
-    SidebarFooter,
-    SidebarContent,
-} from 'react-pro-sidebar';
-import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart, FaReact } from 'react-icons/fa';
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+import {
+  FaTachometerAlt,
+  FaGem,
+  FaList,
+  FaGithub,
+  FaRegLaughWink,
+  FaHeart,
+  FaReact,
+} from "react-icons/fa";
 import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+import logoImg from "../../assests/logo.png";
 //https://react-icons.github.io/react-icons/
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Sidebar.scss'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Sidebar.scss";
 
 const SideBar = (props) => {
-    const { image } = props;
-    // const{handleToggleSidebar} = props;
-    const [collapsed, setCollapsed] = useState(false);
+  const { image } = props;
+  // const{handleToggleSidebar} = props;
+  const [collapsed, setCollapsed] = useState(false);
 
-    const [toggled, setToggled] = useState(false);
-    const [tfReactIcon, settfReactIcon] = useState(true);
+  const [toggled, setToggled] = useState(false);
+  const [tfReactIcon, settfReactIcon] = useState(true);
 
-    const handleCollapsedChange = () => {
-        setCollapsed(!collapsed);
-        settfReactIcon(!tfReactIcon);
-    };
+  const handleCollapsedChange = () => {
+    setCollapsed(!collapsed);
+    settfReactIcon(!tfReactIcon);
+  };
 
-    return (
-        <>
-            <ProSidebar
-                // image={sidebarBg}
-                collapsed={collapsed}
-                toggled={toggled}
-                
-                // handleToggleSidebar={handleToggleSidebar}
-                // handleCollapsedChange={handleCollapsedChange}
+  return (
+    <>
+      <ProSidebar
+        // image={sidebarBg}
+        collapsed={collapsed}
+        toggled={toggled}
+        className="side"
+        style={{ backgroundColor: "#0A092D" }}
+        // handleToggleSidebar={handleToggleSidebar}
+        // handleCollapsedChange={handleCollapsedChange}
+      >
+        <SidebarHeader>
+          <Menu>
+            <MenuItem
+              onClick={handleCollapsedChange}
+              icon={collapsed ? <GoArrowRight /> : <GoArrowLeft />}
             >
+              <div
+                style={{
+                  padding: "9px",
+                  // textTransform: "uppercase",
+                  fontWeight: "bold",
+                  fontSize: 14,
+                  letterSpacing: "1px",
+                }}
+              >
+                Quizone
+              </div>
+            </MenuItem>
+          </Menu>
+          <Menu className={tfReactIcon ? "reactIconFa" : ""}>
+            <MenuItem>
+              <img
+              className="logoimg"
+                src={logoImg}
+                alt="Logo"
+                style={{
+                    marginLeft: "3.6px",
+                  width: "100px",
+                  height: "auto",
+                  display: "block",
+                  margin: "0 auto",
+                }}
+              />
+            </MenuItem>
+          </Menu>
+        </SidebarHeader>
 
-                <SidebarHeader>
-                    <Menu>
+        <SidebarContent>
+          <Menu iconShape="square">
+            <MenuItem
+              icon={<FaHome />}
+              suffix={<span className="badge red">Item1</span>}
+            >
+              <Link to="/" className="nav-link">
+                Trang chủ
+              </Link>
+            </MenuItem>
+            <MenuItem icon={<FaFolderOpen />}> Flashcard của bạn </MenuItem>
+          </Menu>
+          <Menu iconShape="circle">
+            <SubMenu
+              icon={<FaRegLaughWink />}
+              suffix={<span className="badge yellow">3</span>}
+              title="Admin Manage"
+            >
+              <MenuItem>
+                <Link to="User" className="nav-link">
+                  User
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link to="Product" className="nav-link">
+                  Product
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link to="manage-question" className="nav-link">
+                  Item6
+                </Link>
+              </MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
 
-                        <MenuItem
-                            onClick={handleCollapsedChange}
-                            icon={collapsed ? <GoArrowRight /> : <GoArrowLeft />}
-                        >
-                            <div
-                                style={{
-                                    padding: "9px",
-                                    // textTransform: "uppercase",
-                                    fontWeight: "bold",
-                                    fontSize: 14,
-                                    letterSpacing: "1px"
-                                }}
-                            >
-                                Quizone
-                            </div>
-                        </MenuItem>
+        <SidebarFooter style={{ textAlign: "center" }}>
+          <Menu>
+            <MenuItem>
+              <a
+                href="https://github.com/azouaoui-med/react-pro-sidebar"
+                target="_blank"
+                className="sidebar-btn"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+                <span
+                  style={{
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                  }}
+                >
+                  Item7
+                </span>
+              </a>
+            </MenuItem>
+          </Menu>
+        </SidebarFooter>
+      </ProSidebar>
+    </>
+  );
+};
 
-
-                    </Menu>
-                    <Menu className={tfReactIcon ? 'reactIconFa' : ''}>
-                        <MenuItem
-                            icon={<FaReact color='rgb(18 199 241)' size={'2.5em'} />}>
-                        </MenuItem>
-                    </Menu>
-
-                </SidebarHeader>
-
-                <SidebarContent>
-                    <Menu iconShape='square'>
-                        <MenuItem icon={<FaTachometerAlt />} suffix={<span className="badge red" >Item1</span>}>
-                             <Link to='/' className='nav-link'>Home</Link>
-                        </MenuItem>
-                        <MenuItem icon={<FaGem />}> React JS </MenuItem>
-                    </Menu>
-                    <Menu iconShape="circle">
-
-                        <SubMenu icon={<FaRegLaughWink />} suffix={<span className="badge yellow">3</span>} title='Admin Manage'>
-                            <MenuItem>                            
-                                <Link to='User' className='nav-link'>User</Link>
-                            </MenuItem>
-                            <MenuItem>
-                                <Link to='Product' className='nav-link'>Product</Link>
-                            </MenuItem>
-                            <MenuItem>
-                                <Link to='manage-question' className='nav-link'>Item6</Link>
-                            </MenuItem>
-                        </SubMenu>
-
-                    </Menu>
-                </SidebarContent>
-
-                <SidebarFooter style={{ textAlign: 'center' }}>
-                    <Menu>
-                        <MenuItem>
-                            <a
-                                href="https://github.com/azouaoui-med/react-pro-sidebar"
-                                target="_blank"
-                                className="sidebar-btn"
-                                rel="noopener noreferrer">
-                                <FaGithub />
-                                <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
-                                Item7
-                                </span>
-                            </a>
-                        </MenuItem>
-                    </Menu>
-                </SidebarFooter>
-            </ProSidebar>
-        </>
-    )
-}
-
-export default SideBar
+export default SideBar;
