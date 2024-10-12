a0b4e19553e2d897966d415187c8afe42a023137
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2024-10-12 12:31:12 +0700

    kast

--- flashcardfe/src/context/SocketContext.js
+++ flashcardfe/src/context/SocketContext.js
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://quizonebe.onrender.com/", {
+			const socket = io("https://quizonebe.onrender.com", {
 				query: {
 					userId: authUser.access_token,
 				},
