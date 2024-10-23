cee54c31f518de55833edd4ca20bee470a491a6a
de180334 <khoa2k4hg@gmail.com>
2024-10-23 11:16:46 +0700

    responsive , homepage

--- flashcardfe/src/components/Question Pack/ListQuestionPack.js
+++ flashcardfe/src/components/Question Pack/ListQuestionPack.js
@@ -2,7 +2,7 @@ import { Button, Card, Carousel } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAllQuestionPack, getUserByUserId } from "../../service/ApiService";
-import {  useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import "./ListQuestionPack.scss";
 import MyClass from "../Myclass-homepage/myclass";
 import Footer from "../footer/footer";
@@ -23,7 +23,7 @@ const ListQuestionPack = () => {
         response.data.map(async (pack) => {
           if (pack.teacher && pack.teacher._id) {
             const userResponse = await getUserByUserId(pack.teacher._id);
-            userNames[pack.teacher._id] = userResponse.data.username;
+            userNames[pack.teacher._id] = userResponse.data.username || pack.teacher.email;
           } else {
             userNames[pack.teacher ? pack.teacher._id : "unknown"] = "Unknown";
           }
@@ -37,81 +37,87 @@ const ListQuestionPack = () => {
     fetchQuestionPacks();
   }, []);
 
-  // Create slides in groups of 3
-  const slides = [];
-  for (let i = 0; i < questionPackData.length; i += 3) {
-    slides.push(questionPackData.slice(i, i + 3));
+
+  const groupedBySemester = {};
+  for (let semester = 1; semester <= 9; semester++) {
+    groupedBySemester[`ky${semester}`] = questionPackData.filter(pack => pack.semester === `ky${semester}`);
   }
 
-  return (
-    
-    <div className="ListQuestionPack-container">
-      <h2 className="questionpack-h2">Flashcard</h2>
-      {questionPackData.length > 0 ? (
-        <Carousel
-          id="questionPackCarousel"
-          interval={3000}
-          controls
-          indicators={false}
-        >
-          {slides.map((slide, slideIndex) => (
-            <Carousel.Item key={slideIndex}>
-              <div className="row">
-                {slide.map((pack) => (
-                  <div className="col-md-4" key={pack._id}>
-                    <Card
-                      className="mb-3"
-                      style={{   backgroundColor: "#1E1E40",
-                      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-                    }}
-                      onClick={() => {
-                        navigate(`/detailquespack/${pack._id}`, {
-                          state: {
-                            packName: pack.title,
-                            packImagePreview: pack.imagePreview,
-                            packSemester: pack.semester,
-                          },
-                        });
-                      }}
-                    >
-                      <div className="row no-gutters">
-                        <div className="col-md-4">
-                          <Card.Img src={pack.imagePreview} alt={pack.name} />
-                        </div>
-                        <div className="col-md-8">
-                          <Card.Body>
-                            <h5
-                              className="card-title"
-                              style={{ color: "White" }}
-                            >
-                              {pack.title}
-                            </h5>
 
-                            <div className="price">
-                              <span className="user-name">
-                                {userNames[pack.teacher?._id] || "Loading..."}
-                              </span>
-                              <button className="btn btn-secondary btn-queslength">
-                                {pack.questions.length} Flashcard
-                              </button>
+  const createSlides = (packs) => {
+    const slides = [];
+    for (let i = 0; i < packs.length; i += 3) {
+      slides.push(packs.slice(i, i + 3));
+    }
+    return slides;
+  };
+
+  return (
+    <div className="ListQuestionPack-container">
+      <h2 className="questionpack-h2">Flashcard by Semester</h2>
+      {Object.keys(groupedBySemester).map((semester) => (
+        <div key={semester}>
+          <h3 className="semester-title mx-5">Semester {semester.replace('ky', '')}</h3>
+          {groupedBySemester[semester].length > 0 ? (
+            <Carousel
+              id={`questionPackCarousel-semester-${semester}`}
+              interval={3000}
+              controls
+              indicators={false}
+            >
+              {createSlides(groupedBySemester[semester]).map((slide, slideIndex) => (
+                <Carousel.Item key={slideIndex}>
+                  <div className="row">
+                    {slide.map((pack) => (
+                      <div className="col-md-4" key={pack._id}>
+                        <Card
+                          className="mb-3"
+                          style={{ backgroundColor: "#1E1E40", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}
+                          onClick={() => {
+                            navigate(`/detailquespack/${pack._id}`, {
+                              state: {
+                                packName: pack.title,
+                                packImagePreview: pack.imagePreview,
+                                packSemester: pack.semester,
+                              },
+                            });
+                          }}
+                        >
+                          <div className="row no-gutters">
+                            <div className="col-md-4">
+                              <Card.Img src={pack.imagePreview || 'default_image_path.jpg'} alt={pack.title} />
                             </div>
-                          </Card.Body>
-                        </div>
+                            <div className="col-md-8">
+                              <Card.Body>
+                                <h5 className="card-title" style={{ color: "White" }}>
+                                  {pack.title}
+                                </h5>
+                                <div className="price">
+                                  <span className="user-name">
+                                    {userNames[pack.teacher?._id] || "Unknown"}
+                                  </span>
+                                  <button className="btn btn-secondary btn-queslength">
+                                    {pack.questions.length} Flashcard{pack.questions.length > 1 ? 's' : ''}
+                                  </button>
+                                </div>
+                              </Card.Body>
+                            </div>
+                          </div>
+                        </Card>
                       </div>
-                    </Card>
+                    ))}
                   </div>
-                ))}
-              </div>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      ) : (
-        <p>No question packs available.</p>
-      )}
-      <Banner></Banner>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          ) : (
+            <p className=" mx-6 no-questionpack">No question packs available for Semester {semester.replace('ky', '')}.</p>
+          )}
+        </div>
+      ))}
+      <Banner />
       {isAuthenticated ? <MyClass /> : <div></div>}
-     
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
